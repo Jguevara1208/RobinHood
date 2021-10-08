6bcad92828e7a444096c22dd170a7d3865d70d24
Jordan <jordansacct@gmail.com>
2021-10-08 09:20:26 -0700

    finished listSymbol table

--- app/models/list_symbols.py
+++ app/models/list_symbols.py
@@ -3,4 +3,7 @@ from .db import db
 class ListSymbol(db.Model):
     __tablename__ = 'list_symbols'
     id = db.Column(db.Integer, primary_key=True)
-    list_id = db.Column(db.Integer, db.foreignKey('lists.id'))
+    list_id = db.Column(db.Integer, db.foreignKey('lists.id'), nullable=False)
+    symbol = db.Column(db.String(50), nullable=False)
+
+    list = db.relationship('List', back_populates='list_symbols')
