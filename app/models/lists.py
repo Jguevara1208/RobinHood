2a09289dcf6718788ce1e4f5086586430020ec8e
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2021-10-08 09:42:36 -0700

    Worked on list table

---
+++ app/models/lists.py
@@ -0,0 +1,19 @@
+from .db import db
+
+class List(db.Model):
+    __tablename__ = "lists"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.foreignKey("users.id"), nullable=False)
+    list_name = db.Column(db.String(50), nullable=False)
+    
+    #relationship between the list_symbols and list databases
+    list_symbols = db.relationship('ListSymbols', back_populates = 'list', cascade = 'all, delete')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'list_name': self.list_name,
+            'symbols': [symbol['symbol'] for symbol in self.list_symbols]
+        }
