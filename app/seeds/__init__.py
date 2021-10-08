ff187f3b3d38ec9e08291b5ebf53f3ade2ed0502
Jordan <jordansacct@gmail.com>
2021-10-08 10:50:30 -0700

    added most seed to __int__ seed

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,8 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .list_symbols import seed_list_symbols, undo_list_symbols
+from .lists import seed_lists, undo_lists
+from .assets import seed_assets, undo_assets
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -11,6 +13,8 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_assets()
+    seed_lists()
     seed_list_symbols()
     # Add other seed functions here
 
@@ -18,6 +22,8 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
     undo_list_symbols()
+    undo_assets()
+    undo_lists()
+    undo_users()
     # Add other undo functions here
