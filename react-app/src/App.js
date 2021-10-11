f783f7d702b80d2b094ffbcc1f8884a1d5c8c801
Chase Kessler <chasekessler30@yahoo.com>
2021-10-11 12:10:11 -0400

    making new branch

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
@@ -15,11 +15,13 @@ import { setTheme } from './store/theme'
 import { setUserAssets } from './store/userAssets' //id
 import { setUserLists } from './store/userLists'
 import { setWatchListStocks } from './store/watchlistStocks' //symbols
+import MainGraph from './components/MainGraph/MainGraph';
 
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
+  const graphData = useSelector(state => state.currentStock.graphData)
 
   useEffect(() => {
     (async() => {
@@ -56,7 +58,7 @@ function App() {
           <User />
         </ProtectedRoute>
         <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
+          <MainGraph graphData={graphData}/>
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
---
+++ react-app/src/components/MainGraph/MainGraph.js
@@ -0,0 +1,10 @@
+import { ResponsiveContainer, AreaChart, XAxis, YAxis, Area, Tooltip, CartesianGrid } from 'recharts'
+
+
+function MainGraph(graphData){
+    return (
+        <h1>Main Graph</h1>
+    )
+}
+
+export default MainGraph
