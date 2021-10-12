31385916ff31a0eea42234e446eff67a86d419a9
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2021-10-12 11:16:40 -0700

    Worked on the spash page some more

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -3,9 +3,26 @@ import './SplashPage.css'
 import { Link } from 'react-router-dom'
 
 export default function Index() {
+
+    const [visibleTool, setVisibleTool] = React.useState(0)
+
+
+    function increaseVisibleToolValue () {
+        if (visibleTool < 2 ){
+            console.log(visibleTool)
+            setVisibleTool((visibleTool) => visibleTool + 1)
+        }
+    }
+
+    function decreaseVisibleToolValue () {
+        if (visibleTool > 0) {
+            setVisibleTool((visibleTool) => visibleTool - 1)
+        }
+    }
+
     return (
         <div>
-            <div class="container-1">
+            <div class="investing-for-everyone-container">
                 <div class="subcontainer-1">
                     <h1>Investing for Everyone</h1>
                     <span>
@@ -18,35 +35,33 @@ export default function Index() {
                 </div>
             <img src={"https://pamplinmedia.com/images/artimg/00003641830392-0652.jpg"} alt={"We are all investors"}></img>
             </div>
-            <div class="container-2">
-                <img src={"https://pamplinmedia.com/images/artimg/00003641830392-0652.jpg"} alt={"We are all investors"}></img>
-                <div class="subcontainer-2">
-                    <h1>We are all investors</h1>
-                    <Link>See the campaign</Link>
-                </div>
-            </div>
-            <div>
-                <img></img>
-                <div>
-                    <h1>Introducing IPO Access</h1>
-                    <p>Get in at the IPO price. Now you can become one of the first public investors in upcoming IPOs.</p>
+
+            <div className="ipo-access-main-container">
+
+                <img className="ipo-main-img" src="https://robinhood.com/us/en/_next/static/images/balloon__ef7d8a9bb1c7845fcb7a6799c35d513e.svg"></img>
+
+                <div className="ipo-container">
                     <div>
-                        <div>
-                            <img></img>
+                        <h1>Introducing IPO Access</h1>
+                        <p>Get in at the IPO price. Now you can become one of the first public investors in upcoming IPOs.</p>
+                    </div>
+                    <div className="ipo-info-container-main">
+                        <div className="ipo-info-container">
+                            <img className="ipo-info-img" src="https://robinhood.com/us/en/_next/static/images/comeall__c29b103566f44e51d624989e65ecf3be.svg"></img>
                             <div>
                                 <h3>It's your turn</h3>
                                 <p>No minimum account balances or special status requirements</p>
                             </div>
                         </div>
-                        <div>
-                            <img></img>
+                        <div className="ipo-info-container">
+                            <img className="ipo-info-img" src="https://robinhood.com/us/en/_next/static/images/one-first__d86b9ee63a8475364159f2d21ea5f01f.svg"></img>
                             <div>
                                 <h3>Be one of the first</h3>
                                 <p>Request shares in new companies before their stock starts trading on public exchanges</p>
                             </div>
                         </div>
-                        <div>
-                            <img></img>
+                        <div className="ipo-info-container">
+                            <img className="ipo-info-img" src="https://robinhood.com/us/en/_next/static/images/fair-shot__fb09db580d0ada2e8626a6e46094bb27.svg"></img>
                             <div>
                                 <h3>Get a fair shot</h3>
                                 <p>While IPO shares are limited, IPO Access gives you the same opportunity to invest, regardless of order size or account value</p>
@@ -58,48 +73,73 @@ export default function Index() {
 
                 </div>
             </div>
-            <div class="container-3">
+
+            <div class="fractional-shares-container">
                 <div>
                     <h1>Introducing Fractional Shares</h1>
                     <h3>Invest in thousands of stocks, with just 1$</h3>
                 </div>
                 <div class="subcontainer-3">
-                    <div>
+                    <div className="fractional-shares-info">
                         <h4>Invest Any Amount</h4>
                         <p>Choose how much you want to invest, and we’ll convert from dollars to parts of a whole share.</p>
                     </div>
-                    <div>
+                    <div className="fractional-shares-info">
                         <h4>Build a balanced Portfolio</h4>
                         <p>Customize your portfolio with pieces of different companies and funds to help reduce risk.</p>
                     </div>
-                    <div>
+                    <div className="fractional-shares-info">
                         <h4>Trade in real time</h4>
                         <p>Trades placed during market hours are executed at that time, so you’ll always know the share price.</p>
                     </div>
+                    <img className="fractional-shares-side-img" src="https://robinhood.com/us/en/_next/static/images/1x__1358464522fc1bd88f9bf62a40f42192.png"></img>
                 </div>
             </div>
 
             <div className="container-4">
-                <div></div>
+                <div className="tools-buttons-container">
+                    <button onClick={increaseVisibleToolValue}>up</button>
+                    <button onClick={e => setVisibleTool(0)}>Learn</button>
+                    <button onClick={e => setVisibleTool(1)}>Manage</button>
+                    <button onClick={e => setVisibleTool(2)}>Customize</button>
+                    <button onClick={decreaseVisibleToolValue}>Down</button>
+                </div>
                 <div>
+                    {
+                        visibleTool === 0 && (
+                            <div className="info-container">
+                            <img></img>
+                            <div>
+                                <h1>Learn As You Go</h1>
+                                <p>Our goal is to make investing in financial markets more affordable, more intuitive, and more fun, no matter how much experience you have (or don’t have).</p>
+                                </div>
+                            </div>
+                        )
+                    }
 
-                    <img></img>
-                    <div>
-                        <h1>Learn As You Go</h1>
-                        <p>Our goal is to make investing in financial markets more affordable, more intuitive, and more fun, no matter how much experience you have (or don’t have).</p>
-                    </div>
-                    
-                    <img></img>
-                    <div>
-                        <h1>Manage Your Portfolio</h1>
-                        <p>Our goal is to make investing in financial markets more affordable, more intuitive, and more fun, no matter how much experience you have (or don’t have).</p>
-                    </div>
-                    
-                    <img></img>
-                    <div>
-                        <h1>Keep Tabs on Your Money</h1>
-                        <p>Set up customized news and notifications to stay on top of your assets as casually or as relentlessly as you like. Controlling the flow of info is up to you.</p>
-                    </div>
+                    {
+                        visibleTool === 1 && (
+                            <div className="info-container">
+                                <img></img>
+                                <div>
+                                    <h1>Manage Your Portfolio</h1>
+                                    <p>Keep your portfolio in your pocket. Everything you need to manage your assets is available in a single app.</p>
+                                </div>
+                            </div>
+                        )
+                    }
+
+                    {
+                        visibleTool === 2 && (
+                            <div className="info-container">
+                                <img></img>
+                                <div>
+                                    <h1>Keep Tabs on Your Money</h1>
+                                    <p>Set up customized news and notifications to stay on top of your assets as casually or as relentlessly as you like. Controlling the flow of info is up to you.</p>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
 
