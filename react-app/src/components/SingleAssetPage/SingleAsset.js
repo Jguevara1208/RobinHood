1df7110c5f57c72e0b096ee94a02fcb34f1a9bde
Jordan <jordansacct@gmail.com>
2021-10-11 22:21:10 -0700

    finished most of the functionality for the single asset page, need to continue working on buying and selling logic

--- react-app/src/components/SingleAssetPage/SingleAsset.js
+++ react-app/src/components/SingleAssetPage/SingleAsset.js
@@ -3,23 +3,31 @@ import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom'
 import { setCurrentStock } from "../../store/currentStock";
 import { setCompanyStories } from "../../store/currentStories";
+import { setUserAssets } from "../../store/userAssets";
 import MainGraph from "../MainGraph/MainGraph";
 import ResolutionButtons from "../ResolutionButtons/ResolutionButtons";
 import KeyStats from "../KeyStats/KeyStats";
 import StockStories from "../StockStories/StockStories";
+import UserAssetStats from "../UserAssetStats/UserAssetStats";
+import BuySellStocks from "../BuySellStocks/BuySellStocks";
 
 function SingleAsset() {
   const dispatch = useDispatch()
   const {symbol} = useParams()
 
   const {graphData, stockInfo, stockStats, currentPrice} = useSelector(state => state.currentStock)
+  const assets = useSelector(state => state.userAssets)
   const stories = useSelector(state => state.stories)
+  const userId = useSelector(state => state.session.id)
+
   const [resolution, setResolution] = useState('D');
   const [readMore, setReadMore] = useState(false);
 
   const description = stockInfo?.description
   const shortDescription = description?.split(' ').slice(0, 40).join(' ')
   const moreDesctiption = description?.split(' ').slice(40).join(' ')
+
+
 
   const toggleReadMore = () => {
     setReadMore(!readMore)
@@ -28,6 +36,7 @@ function SingleAsset() {
   useEffect(()=>{
     (async ()=>{
       await dispatch(setCompanyStories(symbol))
+      await dispatch(setUserAssets(userId))
     })()
   }, [dispatch])
 
@@ -44,6 +53,12 @@ function SingleAsset() {
         <h2>{stockInfo.name}</h2>
         {graphData && <MainGraph graphData={graphData} />}
         <ResolutionButtons resolution={resolution} setResolution={setResolution} />
+        {assets[symbol] &&
+          <UserAssetStats currentPrice={currentPrice} stockStats={stockStats} assets={assets} symbol={symbol}/>
+        }
+        <div>
+          <BuySellStocks symbol={symbol} price={currentPrice.price}/>
+        </div>
         <div>
           <p>About</p>
           <p>{shortDescription} {!readMore ? '...' : moreDesctiption} {}</p>
