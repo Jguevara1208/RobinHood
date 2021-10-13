03295835bdcb9575972d67ca0c45740f3e159ec8
Chase Kessler <chasekessler30@yahoo.com>
2021-10-13 11:33:55 -0400

    made graphs dynamically change color

--- react-app/src/components/MainGraph/MainGraph.js
+++ react-app/src/components/MainGraph/MainGraph.js
@@ -10,6 +10,9 @@ import '../PortfolioPage/Portfolio.css'
 function MainGraph({graphData, isWatchList=false, isPos}){
   const [hoverPrice, setHoverPrice] = useState(graphData[graphData.length -1].price);
   const [percentDiff, setPercentDiff] = useState(graphData[graphData.length-1]["%"]);
+  // console.log(graphData[graphData.length -1])
+
+  const refPoint = Math.sign(graphData[graphData.length - 1]["%"]);
 
   function handleMouseHover(e){
     if (!isWatchList) {
@@ -27,34 +30,39 @@ function MainGraph({graphData, isWatchList=false, isPos}){
   }
 
   return (
-    <div className='graph-wrapper'>
-        {isWatchList === false && 
+    <div className="graph-wrapper">
+      {isWatchList === false && (
         <div>
-            <Odometer value={hoverPrice} format="(,ddd).dd" />
-            <p className={`${isPos} p-diff`}>{percentDiff}%</p>
+          <Odometer value={hoverPrice} format="(,ddd).dd" />
+          <p className={`${isPos} p-diff`}>{percentDiff}%</p>
         </div>
-        }
+      )}
       {graphData && (
-        <ResponsiveContainer width='100%' height={300} className='graph-container'>
+        <ResponsiveContainer
+          width="100%"
+          height={300}
+          className="graph-container"
+        >
           <LineChart
             data={graphData}
             onMouseMove={handleMouseHover}
             onMouseLeave={resetHoverPrice}
           >
-            <XAxis dataKey="time" hide/>
+            <XAxis dataKey="time" hide />
             <YAxis
               domain={[
                 graphData[0].price - 5,
                 graphData[graphData.length - 1].price + 5,
               ]}
-            hide/>
+              hide
+            />
             <Line
               dataKey="price"
-              stroke="#00C805"
+              stroke={refPoint === 1 ? "#00C805" : "#FE5001"}
               dot={false}
               strokeWidth={2}
             />
-            {isWatchList === false &&
+            {isWatchList === false && (
               <Tooltip
                 wrapperStyle={{
                   borderColor: "white",
@@ -63,14 +71,14 @@ function MainGraph({graphData, isWatchList=false, isPos}){
                 contentStyle={{ backgroundColor: "rgba(255, 255, 255, 0.8)" }}
                 labelStyle={{ fontWeight: "bold", color: "#666666" }}
               />
-            }
+            )}
             <ReferenceLine
               ifOverflow="extendDomain"
               y={graphData[0].price}
               strokeWidth={2}
               strokeHeight={1.5}
               strokeDasharray="1 6"
-              stroke="lightgrey"
+              stroke="#7F7F7F"
             />
           </LineChart>
         </ResponsiveContainer>
